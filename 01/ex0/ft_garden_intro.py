9eec20b0f0f2af32743b2fcada5e44c8ae37fea3
karolochniak <146261022+karolochniak@users.noreply.github.com>
2026-07-09 21:02:24 +0200

    Add files via upload

---
+++ 01/ex0/ft_garden_intro.py
@@ -0,0 +1,11 @@
+def print_hi(type, height, age):
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {type}")
+    print(f"Height: {height}cm")
+    print(f"Age: {age} days")
+    print("=== End of Program ===")# Press F9 to toggle the breakpoint.
+
+if __name__ == '__main__':
+    print_hi("Rose", 25, 30)
+
+
